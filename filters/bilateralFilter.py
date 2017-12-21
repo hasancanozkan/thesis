9b2f44000382be08024310694577a647a58889c9
oezkan <oezkan@LME121.ad.inf5>
2017-12-21 14:49:50 +0100

    anisotropic diffusion file is added, and bilateral filter is set

--- filters/bilateralFilter.py
+++ filters/bilateralFilter.py
@@ -4,25 +4,67 @@ Created on 15.12.2017
 @author: oezkan
 '''
 import cv2
-import numpy as np
 from matplotlib import pyplot as plt
 import time
 
-#img = cv2.imread('2 -##-filteredImage.tif',1) # 1 --> gray scale yapti
-img = cv2.imread('2 -##-filteredImage.tif',0)
-print img.shape
+
+img_nocrack1 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000831_Keincrack.tif",0)
+img_crack1 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000231_crack.tif",0)
+img_crack2 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000281_crack.tif",0)
+img_crack3 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000001220_crack.tif",0)
 
 start_time1 = time.time()
-blur = cv2.bilateralFilter(img,5,75,75)
+bl_nocrack1 = cv2.bilateralFilter(img_nocrack1,5,75,75)
+print(time.time() - start_time1)
+
+start_time1 = time.time()
+bl_crack1 = cv2.bilateralFilter(img_crack1,5,75,75)
+print(time.time() - start_time1)  
+
+start_time1 = time.time()
+bl_crack2 = cv2.bilateralFilter(img_crack2,5,75,75)
+print(time.time() - start_time1) 
+
+start_time1 = time.time()
+bl_crack3 = cv2.bilateralFilter(img_crack3,5,75,75)
 print(time.time() - start_time1) 
 
 
-start_time2 = time.time()
-plt.subplot(131),plt.imshow(img,"gray"),plt.title('Original')
+
+# Original images
+plt.subplot(3,4,1),plt.imshow(img_nocrack1,"gray"),plt.title('Original_nocrack')
 plt.xticks([]), plt.yticks([])
-plt.subplot(132),plt.imshow(blur,"gray"),plt.title('Bilateral')
+plt.subplot(3,4,2),plt.imshow(img_crack1,"gray"),plt.title('Bilateral')
 plt.xticks([]), plt.yticks([])
-plt.subplot(133),plt.imshow(img-blur,"gray"),plt.title('Bilateral')
+plt.subplot(3,4,3),plt.imshow(img_crack2,"gray"),plt.title('Bilateral')
 plt.xticks([]), plt.yticks([])
-plt.show()
-print(time.time() - start_time2) 
+plt.subplot(3,4,4),plt.imshow(img_crack3,"gray"),plt.title('Bilateral')
+plt.xticks([]), plt.yticks([])
+
+#Bilateral Filter
+plt.subplot(3,4,5),plt.imshow(bl_nocrack1,"gray"),plt.title('Original_nocrack')
+plt.xticks([]), plt.yticks([])
+plt.subplot(3,4,6),plt.imshow(bl_crack1,"gray"),plt.title('Bilateral_crack1')
+plt.xticks([]), plt.yticks([])
+plt.subplot(3,4,7),plt.imshow(bl_crack2,"gray"),plt.title('Bilateral_crack2')
+plt.xticks([]), plt.yticks([])
+plt.subplot(3,4,8),plt.imshow(bl_crack3,"gray"),plt.title('Bilateral_crack2')
+plt.xticks([]), plt.yticks([])
+
+#Differences
+plt.subplot(3,4,9),plt.imshow(img_nocrack1 - bl_nocrack1,"gray"),plt.title('Difference')
+plt.xticks([]), plt.yticks([])
+plt.subplot(3,4,10),plt.imshow(img_crack1 - bl_crack1,"gray"),plt.title('Difference')
+plt.xticks([]), plt.yticks([])
+plt.subplot(3,4,11),plt.imshow(img_crack2 - bl_crack2,"gray"),plt.title('Difference')
+plt.xticks([]), plt.yticks([])
+plt.subplot(3,4,12),plt.imshow(img_crack3 - bl_crack3,"gray"),plt.title('Difference')
+plt.xticks([]), plt.yticks([])
+
+plt.show() 
+
+#save images
+cv2.imwrite('bl_nocrack.tif',bl_nocrack1)
+cv2.imwrite('bl_crack1.tif',bl_crack1)
+cv2.imwrite('bl_crack2.tif',bl_crack2)
+cv2.imwrite('bl_crack3.tif',bl_crack3)
