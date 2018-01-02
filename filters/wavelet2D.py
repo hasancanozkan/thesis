2c2252785b7866a09c42ff8563809f1201143cbf
oezkan <oezkan@LME121.ad.inf5>
2018-01-02 19:31:54 +0100

    wavelet results are added

--- filters/wavelet2D.py
+++ filters/wavelet2D.py
@@ -8,7 +8,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 import time
-from numpy import imag
 
 def mad(arr):
     """ Median Absolute Deviation: a "Robust" version of standard deviation.
@@ -24,25 +23,26 @@ img_crack1 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000231_crack.
 img_crack2 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000281_crack.tif",0)
 img_crack3 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000001220_crack.tif",0)
 
-
-image = np.float32(img_nocrack1)
-image /= 255
+# convert to float32
+img_nocrack1 = np.float32(img_nocrack1)
+img_nocrack1 /= 255
 
 start_time1 = time.time()
 #2D multilevel decomposition
 level = 1
-coeffs= pywt.wavedec2(image, wavelet='haar',level=level) 
+wavelet = 'haar'
+coeffs_nocrack1_haar= pywt.wavedec2(img_nocrack1, wavelet=wavelet,level=level) 
 
 #calculate the threshold
-sigma = mad(coeffs[-level])
-threshold = sigma*np.sqrt( 2*np.log(image.size)) 
-newCoeffs = map (lambda x: pywt.threshold(x,threshold,mode='soft'),coeffs)
+sigma = mad(coeffs_nocrack1_haar[-level])
+threshold_haar = sigma*np.sqrt( 2*np.log(img_crack1.size)) #this is soft thresholding
+#threshold_haar = 0.085
+newCoeffs_nocrack1_map_haar = map (lambda x: pywt.threshold(x,threshold_haar,mode='soft'),coeffs_nocrack1_haar)
 
 
-recon_img = pywt.waverec2(newCoeffs, wavelet='haar')
-recon_img *= 255
-recon_img=np.uint8(recon_img)
-
-print(time.time() - start_time1)
-
-cv2.imwrite('wavepic.tif',recon_img)
+newCoeffs_nocrack1_map_haar = pywt.waverec2(newCoeffs_nocrack1_map_haar, wavelet=wavelet)
+newCoeffs_nocrack1_map_haar *= 255
+newCoeffs_nocrack1_map_haar=np.uint8(newCoeffs_nocrack1_map_haar)
+print(time.time() - start_time1) 
+#save the images
+cv2.imwrite('wave_nocrack1_map_haar.tif',newCoeffs_nocrack1_map_haar)
