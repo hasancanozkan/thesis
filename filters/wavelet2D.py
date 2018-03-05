a7247a78d75ecba35af734767d7b68c5bf3e9292
oezkan <oezkan@LME121.ad.inf5>
2018-03-05 09:09:29 +0100

    cleaning the codes in wawelet2D and ROI_function

--- filters/wavelet2D.py
+++ filters/wavelet2D.py
@@ -8,7 +8,7 @@ import numpy as np
 import cv2
 import time
 #from PIL import Image # use to show float32 images
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 
 def mad(arr):
@@ -25,11 +25,9 @@ img_nocrack1 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000831_Kein
 img_crack1 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000231_crack.tif",0)
 img_crack2 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000000281_crack.tif",0)
 img_crack3 = cv2.imread("D://oezkan/Data/MASTERTHESIS_EL_start/0000001220_crack.tif",0)
-print np.max(img_1MB)
 # convert to float32  there is no change if we dont do this!!
 #img = np.float32(img_crack1)
 img = (img_1MB/255.0)
-print np.max(img)
 
 #2D multilevel decomposition
 level = 2
@@ -37,14 +35,14 @@ wavelet = 'haar'
  
 #start_time1 = time.time()
 #decompose to 2nd level coefficients
-[cA2,(cH2, cV2, cD2), (cH1, cV1, cD1)] =  pywt.wavedec2(img_1MB, wavelet=wavelet,level=level)
-coeffs = [cA2,(cH2, cV2, cD2)]
+coeffs=  pywt.wavedec2(img_1MB, wavelet=wavelet,level=level)
+#coeffs = [cA2,(cH2, cV2, cD2)]
 
 
 #calculate the threshold
 sigma = mad(coeffs[-level])
 #sigma = 75
-threshold = sigma*np.sqrt( 2*np.log(img_crack1.size/2)) #this is soft thresholding
+threshold = sigma*np.sqrt( 2*np.log(img_crack1.size/2))
 #print threshold
 #threshold = 50
 newCoeffs = map (lambda x: pywt.threshold(x,threshold,mode='hard'),coeffs)
@@ -53,21 +51,19 @@ newCoeffs = map (lambda x: pywt.threshold(x,threshold,mode='hard'),coeffs)
 #reconstruction
 #recon_img = pywt.waverec2(newCoeffs, wavelet=wavelet)
 recon_img= pywt.waverec2(coeffs, wavelet=wavelet)
-print np.max(recon_img)
 
 # normalization to convert uint8
 normalizedImg = cv2.normalize(recon_img, 0, 255, cv2.NORM_MINMAX)
 normalizedImg *=255
 #print(time.time() - start_time1)
-print np.max(normalizedImg)
 
-"""
+
 #show the chosen image
 plt.imshow(normalizedImg,"gray"),plt.title('111')
 plt.show()
-"""
+
 #save the chosen image
-cv2.imwrite('approx_crack1_hard1.tif', normalizedImg)
+#cv2.imwrite('approx_crack1_hard1.tif', normalizedImg)
 
 
 """
--- segmentation/ROI_function.py
+++ segmentation/ROI_function.py
@@ -9,7 +9,7 @@ import numpy as np
 from random import randint
 
 def createROI(img):
-
+    
     threshold_value = np.min(img) 
     _,ROI = cv2.threshold(img,(threshold_value+30),255,cv2.THRESH_BINARY) #more or less same but
     #_,ROI = cv2.threshold(img,(threshold_value*2),255,cv2.THRESH_BINARY)
@@ -25,36 +25,16 @@ def createROI(img):
     img_morph1 = cv2.morphologyEx(ROI, cv2.MORPH_OPEN, kernel_y)
 
     img_morph = img_morph2 * img_morph1
-    
-    '''
-        the part of the code below is another way of creating ROI
-        I applied blur
-    '''
-    """img = cv2.equalizeHist(img)
-
-
-    threshold_value = np.min(img) 
-    _,img_thr = cv2.threshold(img,threshold_value+50,255,cv2.THRESH_BINARY)
-
-    blur = cv2.blur(img_thr,(9,9))
-
-    _,contours,_ = cv2.findContours(blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    defectImage = np.zeros((img_thr.shape))
-    cv2.drawContours(defectImage, contours, -1, 1, -1)
-    """
-    return img_morph 
-""" 
-    _, contours,_ = cv2.findContours(img_morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    defects = []
-    for i in range(len(contours)):
-            if(cv2.contourArea(contours[i]) > 300):
-                defects.append(contours[i])
-            
-                        
-    defectImage = np.zeros((ROI.shape))
-    cv2.drawContours(defectImage, defects, -1, 1, -1)
-
     return img_morph
-   """ 
+    
+    
+
+    """
+    threshold_value = np.min(img) 
+    _,img_thr = cv2.threshold(img,threshold_value+30,255,cv2.THRESH_BINARY)
+
+    blur = cv2.blur(img_thr,(5,5))
+
+    _, blur = cv2.threshold(blur,250,255,cv2.THRESH_BINARY)
+    return blur
+    """
